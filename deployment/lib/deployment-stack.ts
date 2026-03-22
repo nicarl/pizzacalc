381ce95ce55d16a8600155f6f3e877868d0f333d
Nico <nicocarl@protonmail.com>
2026-03-22 21:13:48 +0100

    feat: simplify deployment

--- deployment/lib/deployment-stack.ts
+++ deployment/lib/deployment-stack.ts
@@ -1,81 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
-import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as s3Deploy from 'aws-cdk-lib/aws-s3-deployment';
-import * as acm from 'aws-cdk-lib/aws-certificatemanager';
-import * as targets from 'aws-cdk-lib/aws-route53-targets';
 import { Construct } from 'constructs';
 
 export class DeploymentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const domainName: string = 'pizzacalculator.net';
-
-    const zone = route53.HostedZone.fromLookup(this, 'Zone', {
-      domainName: domainName,
-    });
-    new cdk.CfnOutput(this, 'Site', { value: 'https://' + domainName });
 
     const bucket = new s3.Bucket(this, 'PizzaCalculatorAppBucket', {
-      bucketName: domainName,
+      bucketName: 'pizzacalculator.net',
       removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
+      publicReadAccess: true,
+      blockPublicAccess: new s3.BlockPublicAccess({
+        blockPublicAcls: true,
+        ignorePublicAcls: true,
+        blockPublicPolicy: false,
+        restrictPublicBuckets: false,
+      }),
     });
+
     new cdk.CfnOutput(this, 'Bucket', { value: bucket.bucketName });
-
-    const certificate = new acm.DnsValidatedCertificate(
-      this,
-      'SiteCertificate',
-      {
-        domainName: domainName,
-        hostedZone: zone,
-        region: 'us-east-1', // CloudFront requires certificates in us-east-1
-      },
-    );
-    new cdk.CfnOutput(this, 'Certificate', {
-      value: certificate.certificateArn,
-    });
-
-    const distribution = new cloudfront.Distribution(
-      this,
-      'PizzaCalculatorAppDistribution',
-      {
-        defaultBehavior: {
-          origin: origins.S3BucketOrigin.withOriginAccessControl(bucket),
-          viewerProtocolPolicy:
-            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-        },
-        domainNames: [domainName],
-        certificate: certificate,
-        defaultRootObject: 'index.html',
-        errorResponses: [
-          {
-            httpStatus: 404,
-            responseHttpStatus: 200,
-            responsePagePath: '/index.html',
-          },
-        ],
-      },
-    );
-    new cdk.CfnOutput(this, 'DistributionId', {
-      value: distribution.distributionId,
-    });
-
-    // Route53 alias record for the CloudFront distribution
-    new route53.ARecord(this, 'SiteAliasRecord', {
-      recordName: domainName,
-      target: route53.RecordTarget.fromAlias(
-        new targets.CloudFrontTarget(distribution),
-      ),
-      zone,
+    new cdk.CfnOutput(this, 'WebsiteURL', {
+      value: bucket.bucketWebsiteUrl,
     });
 
     new s3Deploy.BucketDeployment(this, 'DeployWithInvalidation', {
       sources: [s3Deploy.Source.asset(__dirname + '/../../code/build/')],
       destinationBucket: bucket,
-      distribution,
-      distributionPaths: ['/*'],
     });
   }
 }
